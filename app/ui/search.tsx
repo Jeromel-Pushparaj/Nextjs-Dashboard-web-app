8035747180e06f90898d2b7fc9ef5b56aab598a5
Jeromel Pushparaj <jeromal03@gmail.com>
2024-08-02 21:47:20 +0530

    feat: Pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -10,7 +10,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
 const handleSearch = useDebouncedCallback((term) => {
       console.log(`Searching... ${term}`);
-    const params = new URLSearchParams(searchParams);
+  const params = new URLSearchParams(searchParams);
+      params.set('page', '1');
+
         if (term) {
       params.set('query', term);
     } else {
