2ed6536812ab7c7a1e28dffadd7ef11b93f62ad9
Jeromel Pushparaj <jeromal03@gmail.com>
2024-08-10 07:48:10 +0530

    feat: server side form validation for edit form

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -67,31 +67,40 @@ const UpdateInvoice = FormSchema.omit({ id: true, date: true });
  
 // ...
  
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(
+  id: string,
+  prevState: State,
+  formData: FormData,
+) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
  
-  const amountInCents = amount * 100;
-  try {
-      await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
-   
-  } catch (error) {
+  if (!validatedFields.success) {
     return {
-      message: 'Database Error: Failed to Update Invoice.'};
- }
-
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Invoice.',
+    };
+  }
+ 
+  const { customerId, amount, status } = validatedFields.data;
+  const amountInCents = amount * 100;
+ 
+  try {
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+  } catch (error) {
+    return { message: 'Database Error: Failed to Update Invoice.' };
+  }
  
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
-
 export async function deleteInvoice(id: string) {
     // throw new Error('Failed to Delete Invoice');
 
