b2c624c3a0f68395a58d69d2d538489dbcb86628
Jeromel Pushparaj <jeromal03@gmail.com>
2024-08-10 08:13:36 +0530

    feat: authentication logic completed

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,4 +1,7 @@
 'use server';
+
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
@@ -114,4 +117,23 @@ export async function deleteInvoice(id: string) {
   }
   
 
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
