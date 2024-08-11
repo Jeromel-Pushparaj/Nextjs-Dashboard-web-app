f0a65b3f46d1bee8917ae2cac5d737ec7dfef5f1
Jeromel Pushparaj <jeromal03@gmail.com>
2024-08-11 10:10:17 +0530

    feat: Adding Meta data

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -5,6 +5,11 @@ import { Suspense } from 'react';
 import CardWrapper from '@/app/ui/dashboard/cards';
 import { LatestInvoicesSkeleton, RevenueChartSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 
  
 export default async function Page() {
